5549516d286abcfd86369905319cd888a272e291
chenxiaosen <chenxiaosencn@gmail.com>
2023-06-28 22:41:19 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,9 +2,9 @@ const BLOG = {
   title: 'chenxiaosen',
   author: 'xiaosen chen',
   email: 'chenxiaosencn@gmail.com',
-  link: 'https://nobelium.vercel.app',
+  link: 'https://nobelium-pi-roan.vercel.app/',
   description: 'This gonna be an awesome website.',
-  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
