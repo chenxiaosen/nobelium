57f403c7bc8e39ee626aee14cf6a60324432ae21
cxs <cxs@wepig.cn>
2023-11-13 15:52:23 +0800

    feat:change socialLink

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   // socialLink: 'https://twitter.com/seanchan_',
-  socialLink: 'https://stackoverflow.com/users/22127797/xiaosen-chen?tab=summary',
+  socialLink: 'https://github.com/chenxiaosen',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
